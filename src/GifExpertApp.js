7a9fd14a8a778f90161c494a1e5fabd7beb371dd
Fredyglz <fredyglz.fr@gmail.com>
2021-04-04 23:42:28 -0500

    primer commit

---
+++ src/GifExpertApp.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import AddCategory from './components/AddCategory';
+import GifGrid from './components/GifGrid';
+
+const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['One Punch']);
+
+    // const handleAdd = () => {
+    //     // setCategories(cats => ['Attack of Titans', ...categories]);
+    //     setCategories(['Attack of Titans', ...categories]);
+    // }
+
+    return(
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories} />
+            <hr />
+
+            {/* <button onClick={handleAdd}>Agregar</button> */}
+            <ol>
+                {/* {
+                    categories.map((category) => {
+                        return <li key={category}>{category}</li>
+                    })
+                } */}
+                {
+                    categories.map((category) => {
+                        return <GifGrid
+                                    key={category} 
+                                    category={category} 
+                                />
+                    })
+                }
+            </ol>
+        </>
+    )
+}
+
+export default GifExpertApp;
